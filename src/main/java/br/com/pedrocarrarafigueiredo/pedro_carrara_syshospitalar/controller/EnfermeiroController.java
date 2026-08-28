bf3d799c0e1c6c2b61587804ce14fc2ca53e571a
pedrocarrara12 <pedro.figueiredo@unimedcuiaba.coop.br>
2026-08-28 09:56:17 -0400

    Adiciona OpenAPI e ajustes nos controllers

--- src/main/java/br/com/pedrocarrarafigueiredo/pedro_carrara_syshospitalar/controller/EnfermeiroController.java
+++ src/main/java/br/com/pedrocarrarafigueiredo/pedro_carrara_syshospitalar/controller/EnfermeiroController.java
@@ -1,4 +1,32 @@
 package br.com.pedrocarrarafigueiredo.pedro_carrara_syshospitalar.controller;
 
+import br.com.pedrocarrarafigueiredo.pedro_carrara_syshospitalar.domain.Enfermeiro;
+import br.com.pedrocarrarafigueiredo.pedro_carrara_syshospitalar.service.EnfermeiroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/enfermeiros")
 public class EnfermeiroController {
+    private final EnfermeiroService enfermeiroService;
+
+    public EnfermeiroController(EnfermeiroService enfermeiroService) {
+        this.enfermeiroService = enfermeiroService;
+    }
+    @GetMapping
+    public ResponseEntity<List<Enfermeiro>> buscarTodosEnfermeiros() {
+        List<Enfermeiro> enfermeiroList = enfermeiroService.buscarTodos();
+        return ResponseEntity.ok(enfermeiroList);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Enfermeiro> buscarEnfermeiroPorId(@PathVariable Long id) {
+        Enfermeiro enfermeiro = enfermeiroService.buscarPorId(id);
+        return ResponseEntity.ok(enfermeiro);
+    }
+
 }
--- src/main/java/br/com/pedrocarrarafigueiredo/pedro_carrara_syshospitalar/controller/MedicoController.java
+++ src/main/java/br/com/pedrocarrarafigueiredo/pedro_carrara_syshospitalar/controller/MedicoController.java
@@ -1,4 +1,41 @@
 package br.com.pedrocarrarafigueiredo.pedro_carrara_syshospitalar.controller;
 
+import br.com.pedrocarrarafigueiredo.pedro_carrara_syshospitalar.domain.Medico;
+import br.com.pedrocarrarafigueiredo.pedro_carrara_syshospitalar.service.MedicoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
 public class MedicoController {
+    private final MedicoService medicoService;
+
+    public MedicoController(MedicoService medicoService) {
+        this.medicoService = medicoService;
+    }
+    @GetMapping
+    public ResponseEntity<List<Medico>> buscarTodosMedicos() {
+        List<Medico> medicoList = medicoService.buscarTodos();
+        return ResponseEntity.ok(medicoList);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Medico> buscarPorId(@PathVariable Long id) {
+        Medico medico = medicoService.buscarPorId(id);
+        return ResponseEntity.ok(medico);
+    }
+    @PostMapping
+    public ResponseEntity<Medico> criarMedico(@RequestBody Medico medico) {
+        Medico medicoCadastro = medicoService.cadastrar(medico);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(medicoCadastro);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Medico> atualizarMedico(@PathVariable Long id, Medico medico) {
+        Medico medicoAtualizado = medicoService.atualizar(id,medico);
+        return ResponseEntity.ok(medicoAtualizado);
+    }
+
 }
